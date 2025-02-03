20cb3b79fd9a3b31f69d77db6daf5b81a4417a85
Vox Odinson <macos@MacOSs-MacBook-Air.local>
2025-02-03 09:16:19 +0700

    update student collector

--- pages/student_list.vue
+++ pages/student_list.vue
@@ -1,185 +1,207 @@
 <template>
-    <div 
-        class="flex items-center justify-between h-[45px] px-4 bg-white border-b-[1px] border-gray-200">
-        <UBreadcrumb 
-            :links="linksItem"
-            divider="/"
-            :ui="{
-                base: 'font-semibold text-[.8rem]',
-                inactive: 'hover:text-blue-200',
-                active: 'text-blue-400',}"/>
+    <template v-if="openCreate">
+        <Student
+            :student-id="studentId"
+            @toggle="toggleCreate"
+            @update:data="fetchData"/>
+    </template>
+    <template v-else>
         <div 
-            class=" flex gap-2 items-center justify-center h-full">
-            <UInput
-                icon="material-symbols:search"
-                type="text"
-                color="white"
-                variant="outline"
-                size="sm"
-                name="district"
-                role="input"
-                placeholder="Search name here..."
-                class="w-[400px]"/>
-            <UTooltip 
-                :text="isOpenFilter ? 'Close Filters' : 'Open Filters'"
-                :popper="{ offsetDistance: 12 }">
-                <UButton
-                    :icon="isOpenFilter ? 'material-symbols:close-rounded' : 'material-symbols:filter-alt-outline'"
-                    size="sm"
-                    color="black"
-                    variant="soft" 
-                    :padded="false"
-                    @click="()=>{
-                        toggle();
-                    }"
-                    class="bg-[#3A6D8C] hover:bg-gray-200 text-white hover:text-black p-1.5 transition"/>
-            </UTooltip>
-        </div>
-    </div>
-    <div 
-        class="w-full p-2 ">
-        <div 
-            v-if="isOpenFilter"
-            class="w-full flex flex-col gap-2 bg-white rounded-md p-2 mb-2" >
+            class="flex items-center justify-between h-[45px] px-4 bg-white border-b-[1px] border-gray-200">
+            <UBreadcrumb 
+                :links="linksItem"
+                divider="/"
+                :ui="{
+                    base: 'font-semibold text-[.8rem]',
+                    inactive: 'hover:text-blue-200',
+                    active: 'text-blue-400',}"/>
             <div 
-                class="w-full flex justify-between flex-wrap gap-3">
-                <div 
-                    class="flex w-full flex-wrap gap-2">
-                    <UFormGroup
-                        class="w-[calc(98%/4)]"
-                        label="Departments"
-                        name="">
-                        <SelectMenu
-                            name=""
-                            :options="[]"
-                            value-attribute="id"
-                            option-attribute="name"
-                            id-attribute="id"
-                            placeholder="Select a department"
-                            class="w-full"/>
-                    </UFormGroup>
-                    <UFormGroup
-                        class="w-[calc(98%/4)]"
-                        label="Class"
-                        name="">
-                        <SelectMenu
-                            name=""
-                            :options="[]"
-                            value-attribute="id"
-                            option-attribute="name"
-                            id-attribute="id"
-                            placeholder="Select a class"
-                            class="w-full"/>
-                    </UFormGroup>  
-                    <UFormGroup
-                        class="w-[calc(98%/4)]"
-                        label="Shift Time"
-                        name="">
-                        <SelectMenu
-                            name=""
-                            :options="[]"
-                            value-attribute="id"
-                            option-attribute="name"
-                            id-attribute="id"
-                            placeholder="Select a shift"
-                            class="w-full"/>
-                    </UFormGroup>  
-                    <UFormGroup
-                        class="w-[calc(97.5%/4)]"
-                        label="Nationality"
-                        name="">
-                        <SelectMenu
-                            name=""
-                            :options="[]"
-                            value-attribute="id"
-                            option-attribute="name"
-                            id-attribute="id"
-                            placeholder="Select a nationality"
-                            class="w-full"/>
-                    </UFormGroup>  
-                </div>
+                class=" flex gap-2 items-center justify-center h-full">
+                <UInput
+                    icon="material-symbols:search"
+                    type="text"
+                    color="white"
+                    variant="outline"
+                    size="sm"
+                    name="district"
+                    role="input"
+                    placeholder="Search name here..."
+                    class="w-[400px]"/>
+                <UTooltip 
+                    text="Create new student"
+                    :popper="{ offsetDistance: 12 }">
+                    <UButton
+                        icon="material-symbols:add-circle-outline-rounded"
+                        size="sm"
+                        color="black"
+                        label="New Student"
+                        variant="soft" 
+                        :padded="false"
+                        @click="()=>{
+                            toggleCreate(true);
+                        }"
+                        class="bg-[#3A6D8C] hover:bg-gray-200 text-white hover:text-black p-1.5 transition"/>
+                </UTooltip>
+                <UTooltip 
+                    :text="isOpenFilter ? 'Close Filters' : 'Open Filters'"
+                    :popper="{ offsetDistance: 12 }">
+                    <UButton
+                        :icon="isOpenFilter ? 'material-symbols:close-rounded' : 'material-symbols:filter-alt-outline'"
+                        size="sm"
+                        color="black"
+                        variant="soft" 
+                        :padded="false"
+                        @click="()=>{
+                            toggle();
+                        }"
+                        class="bg-[#3A6D8C] hover:bg-gray-200 text-white hover:text-black p-1.5 transition"/>
+                </UTooltip>
             </div>
         </div>
         <div 
-            class="w-full bg-white rounded-md overflow-hidden">
-            <Table
-                :columns="columns"
-                :data="datas"
-                is-custom
-                v-slot="{ data }"
-                @update:data="async (current_page: number): Promise<void> => {
-                    
-                }">
-                <tr 
-                    class="*:px-2.5 *:py-1.5 hover:bg-gray-100 cursor-pointer">
-                    <td
-                        class="w-[150px]">
-                        <span>
-                            {{ data.stu_id }}
-                        </span>
-                    </td>
-                    <td>
-                        <div class="w-[60px] h-[60px] overflow-hidden rounded-full border-[1px] border-gray-200">
-                            <img 
-                                :src="data.image_url || UserImage" 
-                                alt=""
-                                class="w-full h-full object-cover">
-                        </div>
-                    </td>
-                    <td>
-                        <span>{{ data.fullname }}</span>
-                    </td>
-                    <td>
-                        <span>{{ data.dob }}</span>
-                    </td>
-                    <td>
-                        <span>{{ data.gender }}</span>
-                    </td>
-                    <td>
-                        <span>{{ data.nationality }}</span>
-                    </td>
-                    <td>
-                        <span
-                            class="text-[.9rem]">
-                            {{ data.class }} 
-                            - <span v-if="data.year">Y{{ data.year }}</span>
-                            <span v-else>-</span>
-                            - {{ data.shift }}
-                        </span>
-                    </td>
-                    <td class="w-[230px]">
-                        <div class="*:text-[.9rem]">
+            class="w-full p-2 ">
+            <div 
+                v-if="isOpenFilter"
+                class="w-full flex flex-col gap-2 bg-white rounded-md p-2 mb-2" >
+                <div 
+                    class="w-full flex justify-between flex-wrap gap-3">
+                    <div 
+                        class="flex w-full flex-wrap gap-2">
+                        <UFormGroup
+                            class="w-[calc(98%/4)]"
+                            label="Departments"
+                            name="">
+                            <SelectMenu
+                                name=""
+                                :options="[]"
+                                value-attribute="id"
+                                option-attribute="name"
+                                id-attribute="id"
+                                placeholder="Select a department"
+                                class="w-full"/>
+                        </UFormGroup>
+                        <UFormGroup
+                            class="w-[calc(98%/4)]"
+                            label="Class"
+                            name="">
+                            <SelectMenu
+                                name=""
+                                :options="[]"
+                                value-attribute="id"
+                                option-attribute="name"
+                                id-attribute="id"
+                                placeholder="Select a class"
+                                class="w-full"/>
+                        </UFormGroup>  
+                        <UFormGroup
+                            class="w-[calc(98%/4)]"
+                            label="Shift Time"
+                            name="">
+                            <SelectMenu
+                                name=""
+                                :options="[]"
+                                value-attribute="id"
+                                option-attribute="name"
+                                id-attribute="id"
+                                placeholder="Select a shift"
+                                class="w-full"/>
+                        </UFormGroup>  
+                        <UFormGroup
+                            class="w-[calc(97.5%/4)]"
+                            label="Nationality"
+                            name="">
+                            <SelectMenu
+                                name=""
+                                :options="[]"
+                                value-attribute="id"
+                                option-attribute="name"
+                                id-attribute="id"
+                                placeholder="Select a nationality"
+                                class="w-full"/>
+                        </UFormGroup>  
+                    </div>
+                </div>
+            </div>
+            <div 
+                class="w-full bg-white rounded-md overflow-hidden">
+                <Table
+                    :columns="columns"
+                    :data="datas"
+                    is-custom
+                    v-slot="{ data }"
+                    @update:data="async (current_page: number): Promise<void> => {
+                        
+                    }">
+                    <tr 
+                        class="*:px-2.5 *:py-1.5 hover:bg-gray-100 cursor-pointer">
+                        <td
+                            class="w-[150px]">
                             <span>
-                                {{ data.phone }}
+                                {{ data.stu_id }}
                             </span>
-                            <span 
-                                class="block">
-                                {{ data.gmail }}
+                        </td>
+                        <td>
+                            <div class="w-[60px] h-[60px] overflow-hidden rounded-full border-[1px] border-gray-200">
+                                <img 
+                                    :src="data.image_url || UserImage" 
+                                    alt=""
+                                    class="w-full h-full object-cover">
+                            </div>
+                        </td>
+                        <td>
+                            <span>{{ data.fullname }}</span>
+                        </td>
+                        <td>
+                            <span>{{ data.dob }}</span>
+                        </td>
+                        <td>
+                            <span>{{ data.gender }}</span>
+                        </td>
+                        <td>
+                            <span>{{ data.nationality }}</span>
+                        </td>
+                        <td>
+                            <span
+                                class="text-[.9rem]">
+                                {{ data.class }} 
+                                - <span v-if="data.year">Y{{ data.year }}</span>
+                                <span v-else>-</span>
+                                - {{ data.shift }}
                             </span>
-                        </div>
-                    </td>
-                    <td>
-                        <UDropdown 
-                            :items="items" 
-                            :popper="{ 
-                                placement: 'bottom-start' 
-                            }">
-                            <UButton 
-                                color="white"
-                                trailing-icon="mdi:dots-vertical" />
-                        </UDropdown>
-                    </td>
-                </tr>
-            </Table>
+                        </td>
+                        <td class="w-[230px]">
+                            <div class="*:text-[.9rem]">
+                                <span>
+                                    {{ data.phone }}
+                                </span>
+                                <span 
+                                    class="block">
+                                    {{ data.gmail }}
+                                </span>
+                            </div>
+                        </td>
+                        <td>
+                            <UDropdown 
+                                :items="[]" 
+                                :popper="{ 
+                                    placement: 'bottom-start' 
+                                }">
+                                <UButton 
+                                    color="white"
+                                    trailing-icon="mdi:dots-vertical" />
+                            </UDropdown>
+                        </td>
+                    </tr>
+                </Table>
+            </div>
         </div>
-    </div>
+    </template>
 </template>
   
 <script lang="ts" setup>
 import { 
     Table,
-    SelectMenu,
-    InputDateRange
+    SelectMenu
 } from '@/components/ui';
 import {
     ContextAPI,
@@ -194,6 +216,9 @@ import type {
 import { 
     UserImage 
 } from '@/assets/images';
+import { 
+    Student
+} from '@/collector/pages';
 definePageMeta({
     colorMode: 'light'
 });
@@ -209,7 +234,16 @@ definePageMeta({
 /**
  * Begin::Declare some variable section
  */
- const isOpenFilter: Ref<boolean> = ref<boolean>(true);
+const dataOptions: Ref<Options> = ref<Options>({});
+const data: Ref<object> = ref<object>({});
+const timeout: Ref<NodeJS.Timeout | null> = ref<NodeJS.Timeout | null>(null);
+const filters: Ref<Items> = ref<Items>({
+    status_id: '',
+    warehouse_id: ''
+});
+const isOpenFilter: Ref<boolean> = ref<boolean>(true);
+const openCreate: Ref<boolean> = ref<boolean>(false);
+const studentId: Ref<number | null> = ref<number | null>(null);
 const linksItem = [
     {
         label: 'Main Menu'
@@ -251,36 +285,7 @@ const columns: Ref<Column[]> = ref<Column[]>([
         title: "Action"
     }
  ]);
- const items = [
-  [{
-    label: 'Profile',
-    avatar: {
-      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-    }
-  }], [{
-    label: 'Edit',
-    icon: 'i-heroicons-pencil-square-20-solid',
-    shortcuts: ['E'],
-    click: () => {
-      console.log('Edit')
-    }
-  }, {
-    label: 'Duplicate',
-    icon: 'i-heroicons-document-duplicate-20-solid',
-    shortcuts: ['D'],
-    disabled: true
-  }], [{
-    label: 'Archive',
-    icon: 'i-heroicons-archive-box-20-solid'
-  }, {
-    label: 'Move',
-    icon: 'i-heroicons-arrow-right-circle-20-solid'
-  }], [{
-    label: 'Delete',
-    icon: 'i-heroicons-trash-20-solid',
-    shortcuts: ['⌘', 'D']
-  }]
-]
+
 const datas = {
     status: 'ok',
     data: [
@@ -354,10 +359,66 @@ const datas = {
  */
 
 /**
+ * Begin::Fetch data section
+ */
+ const fetchData = async (current_page: number = 1, search: string = ''): Promise<void> => {
+    const per_page: number = 10;
+    let url: string = `purchase?per_page=${per_page}&page_no=${current_page}`;
+    if(search)
+    {
+        url += `&search=${search}`;
+    }
+    const result: ResponseStatus = await api.get(url) as ResponseStatus;
+    if(!result.error)
+    {
+        data.value = result as object;
+    }
+}
+
+const fetchOption = async (): Promise<void> => {
+    const options: ResponseStatus = await api.get("") as ResponseStatus;
+    if(!options.error)
+    {
+        dataOptions.value = options.data as unknown as Options;
+    }
+};
+
+const searchData = async (value: string): Promise<void> => {
+    if(timeout.value)
+    {
+        clearTimeout(timeout.value);
+    }
+    timeout.value = setTimeout(async (): Promise<void> => {
+        await fetchData(1, value);
+    }, 250);
+}
+
+const filterData = async (current_page: number = 1): Promise<void> => {
+    const per_page: number = 10;
+    const url: string = `package?per_page=${per_page}&page_no=${current_page}&status_id=${filters.value.status_id}&warehouse_id=${filters.value.warehouse_id}`;
+    const result: ResponseStatus = await api.get(url, false) as ResponseStatus;
+    if(!result.error)
+    {
+        data.value = result as object;
+    }
+}
+/**
+ * End::Fetch data section
+ */
+ watch((): boolean => openCreate.value, async (value: boolean): Promise<void> => {
+    if(!value)
+    {
+        studentId.value = null;
+    }
+});
+/**
  * Begin::Some logical in this component
  */
  const toggle = (): void => {
     isOpenFilter.value = !isOpenFilter.value as boolean;
+}
+const toggleCreate = (value: boolean): void => {
+    openCreate.value = value as boolean;
 }
 /**
  * End::Some logical in this component
