c73290567a8af93df926831936109141d6c66b75
Vox Odinson <macos@MacOSs-MacBook-Air.local>
2025-02-20 23:23:12 +0700

    update result

--- modal/index.ts
+++ modal/index.ts
@@ -1,6 +1,8 @@
 import ExamInfoModal from './exam_modal.vue';
 import NotedModal from './note.vue';
+import ClassResultInfo from './class_result_info.vue';
 export {
     ExamInfoModal,
-    NotedModal
+    NotedModal,
+    ClassResultInfo
 }
